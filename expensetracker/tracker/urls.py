f33840d478f14a305f5fdf9d00f0483b2189319a
Leony Brok <leonybrok@gmail.com>
2018-08-04 20:24:23 +0000

    Added 2 new pages

--- expensetracker/tracker/urls.py
+++ expensetracker/tracker/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("dashboard", views.dashboard, name="dashboard"),
+    path("createcategory", views.createcategory, name="createcategory"),
+    path("addexpense", views.addexpense, name="addexpense"),
     path("history", views.history, name="history")
 ]
--- expensetracker/tracker/views.py
+++ expensetracker/tracker/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 def dashboard(request):
     return render(request, "dashboard.html", {"title": "Dashboard"})
 
+def createcategory(request):
+    return render(request, "createcategory.html", {"title": "Create New Category"})
+
+def addexpense(request):
+    return render(request, "addexpense.html", {"title": "Log an Expense"})
+
 def history(request):
     return render(request, "history.html", {"title": "History"})
