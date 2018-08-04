44e0c3b4fef4f007fad78ee66863f8c0f78d36e5
Leony Brok <leonybrok@gmail.com>
2018-08-04 20:35:57 +0000

    Added empty 'My wallets' page

--- expensetracker/tracker/urls.py
+++ expensetracker/tracker/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("dashboard", views.dashboard, name="dashboard"),
+    path("wallets", views.wallets, name="wallets"),
     path("createcategory", views.createcategory, name="createcategory"),
     path("addexpense", views.addexpense, name="addexpense"),
     path("history", views.history, name="history")
--- expensetracker/tracker/views.py
+++ expensetracker/tracker/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 def dashboard(request):
     return render(request, "dashboard.html", {"title": "Dashboard"})
 
+def wallets(request):
+    return render(request, "wallets.html", {"title": "My Wallets"})
+
 def createcategory(request):
     return render(request, "createcategory.html", {"title": "Create New Category"})
 
