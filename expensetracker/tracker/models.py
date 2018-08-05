ae9379623f415fbadaf2c87803a9a85e0294b63c
Leony Brok <leonybrok@gmail.com>
2018-08-05 21:09:08 +0000

    Create models and make migration

--- expensetracker/tracker/models.py
+++ expensetracker/tracker/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 class Wallet(models.Model):
     name = models.CharField(max_length = 64)
-    #currency
+    currency = models.ForeignKey(
+        'Currency',
+        on_delete = models.PROTECT,
+    )
     date_created = models.DateTimeField(
         'Date Created',
         auto_now_add = True,
@@ -18,7 +21,15 @@ class Wallet(models.Model):
     )
 
     def __str__(self):
-        return f"{self.id}"
+        return f"Wallet {self.id} from {self.user}. Uses {self.currency}. Created on {self.date_created}, last used on {self.last_used}."
+
+class Currency(models.Model):
+    name = models.CharField(max_length = 64)
+    code = models.CharField(max_length = 4)
+    unicode_html = models.CharField(max_length = 64)
+
+    def __str__(self):
+        return f"{self.id} - {self.code} {self.name} Unicode html: {self.unicode_html}."
 
 class Category(models.Model):
     name = models.CharField(max_length = 64)
@@ -33,19 +44,19 @@ class Category(models.Model):
     TEAL = 'teal'
     CYAN = 'cyan'
     COLOR_CHOICES = (
-        (BLUE),
-        (INDIGO),
-        (PURPLE),
-        (PINK),
-        (RED),
-        (ORANGE),
-        (YELLOW),
-        (GREEN),
-        (TEAL),
-        (CYAN),
+        (BLUE, 'Blue'),
+        (INDIGO, 'Indigo'),
+        (PURPLE, 'Purple'),
+        (PINK, 'Pink'),
+        (RED, 'Red'),
+        (ORANGE, 'Orange'),
+        (YELLOW, 'Yellow'),
+        (GREEN, 'Green'),
+        (TEAL, 'Teal'),
+        (CYAN, 'Cyan'),
     )
     color = models.CharField(
-        max_length = 64,
+        max_length = 8,
         choices = COLOR_CHOICES,
         default = 'cyan',
     )
@@ -67,15 +78,44 @@ class Category(models.Model):
     )
 
     def __str__(self):
-        return f"{self.id}"
+        return f"{self.id} - Category {self.name} from wallet {self.wallet}." \
+                f"Color: {self.color}. Icon: {self.icon}. Created on {self.date_created}, last edited on {self.last_edited}."
 
 class Expense(models.Model):
-    amount = models.DecimalField( ###
+    amount = models.DecimalField(
         "What does it cost?",
-        max_digits = 20,
+        max_digits = 32,
         decimal_places = 2,
     )
-    comment = models.CharField(max_length = 255)
+    DEBIT = 'Debit'
+    CREDIT = 'Credit'
+    CASH = 'Cash'
+    ONLINE = 'Online'
+    PAYPAL = 'Paypal'
+    MOBILE = 'Mobile'
+    BANKTRANSFER = 'Transfer'
+    CHEQUE = 'Cheque'
+    OTHER = 'Other'
+    METHOD_CHOICES = (
+        (DEBIT, 'Debit Card'),
+        (CREDIT, 'Credit Card'),
+        (CASH, 'Cash'),
+        (ONLINE, 'Online'),
+        (PAYPAL, 'PayPal'),
+        (MOBILE, 'Mobile'),
+        (BANKTRANSFER, 'Bank Transfer'),
+        (CHEQUE, 'Cheque'),
+        (OTHER, 'Other'),
+    )
+    method = models.CharField(
+        'Payment Method',
+        choices = METHOD_CHOICES,
+        max_length = 8,
+    )
+    comment = models.CharField(
+        max_length = 255,
+        blank = True,
+    )
     date_spent = models.DateTimeField(
         'Date Spent',
     )
@@ -89,7 +129,9 @@ class Expense(models.Model):
     )
     category = models.ForeignKey(
         'Category',
-        on_delete = models.CASCADE, #TODO
+        on_delete = models.SET_NULL,
+        blank = True,
+        null = True,
     )
     wallet = models.ForeignKey(
         'Wallet',
@@ -97,4 +139,6 @@ class Expense(models.Model):
     )
 
     def __str__(self):
-        return f"{self.id}"
+        return f"{self.id} - {self.amount} spent from wallet {self.wallet} in category {self.category}." \
+                f"Paid with {self.method}. Comment: {self.comment}." \
+                f"Spent on {self.date_spent}, created on {self.date_created}, last used on {self.last_edited}."
