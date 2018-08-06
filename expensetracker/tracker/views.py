d27b111b3a94abc4e768a44eaaaabd7504b30ac8
Leony Brok <leonybrok@gmail.com>
2018-08-06 00:45:02 +0000

    User can create categories

--- expensetracker/tracker/views.py
+++ expensetracker/tracker/views.py
@@ -26,6 +26,7 @@ def wallets(request):
                 user = request.user,
             )
             w.save()
+            messages.success(request, "You successfully created a new wallet.")
         except IntegrityError:
             messages.error(request, "Your wallet names must be unique.")
 
@@ -43,9 +44,35 @@ def wallets(request):
     return render(request, "wallets.html", {"title": "Wallets", "currencies": currencies, "wallets": wallets})
 
 def createcategory(request):
+    if request.method == 'POST':
+        name = request.POST["name"]
+        color = request.POST["color"]
+        wallet_id = request.POST["wallet"]
+
+        w = Wallet.objects.get(id = wallet_id)
+
+        # Save category.
+        try:
+            c = Category.objects.create(
+                    name = name,
+                    color = color,
+                    wallet = w,
+                )
+            c.save()
+            messages.success(request, "You successfully added a new category.")
+        except IntegrityError:
+            messages.error(request, "A wallet can't have two categories with the same name.")
+
+    # Wallets for form.
+    wallets = Wallet.objects.filter(user = request.user).order_by('-last_used').values()
+
+    # Query database for categories.
+    categories = Category.objects.filter(wallet = wallets[0]["id"]).values()
+
     # List of colors to choose from.
     colors = ['Blue', 'Indigo', 'Purple', 'Pink', 'Red', 'Orange', 'Yellow', 'Green', 'Teal', 'Cyan']
-    return render(request, "createcategory.html", {"title": "Categories", "colors": colors})
+
+    return render(request, "createcategory.html", {"title": "Categories", "colors": colors, "wallets": wallets, "categories": categories})
 
 def addexpense(request):
     return render(request, "addexpense.html", {"title": "Log an Expense"})
