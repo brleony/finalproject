3438f49e14c6e4ed23522bf4370863639b8d6a78
Leony Brok <leonybrok@gmail.com>
2018-08-09 18:31:40 +0000

    New procfile

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import dj_database_url
 from local_settings import *
 
 from django.contrib.messages import constants as messages
